28d2571cad95e98afbbe98de85ce232d7c3a6955
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-30 09:36:02 +0530

    added page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { FlippingCard } from "@/components/flippingCard";
+import { Logo } from "@/components/logo";
 
 
 export default function Home() {
   return (
-   <div className="bg-background h-screen w-full text-black flex justify-center items-center">
+    <div className="bg-background h-screen w-full text-black flex flex-col justify-between items-center"> 
+      <Logo className="text-text-primary opacity-60 mt-1"/> 
       <FlippingCard />
-   </div>
+      <footer className="text-text-primary font-mono text-sm mb-1">Design & Developed by Prasanjit.</footer>
+    </div>
   );
 }
