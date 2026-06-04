6fe1b7344ee0f23b89b8de0f152dcfd0185cd0d4
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2026-06-04 18:48:53 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ app/blue/page.tsx
@@ -0,0 +1,6 @@
+import { themes } from '@/components/themed/themes';
+import { ThemedPage } from '@/components/themed/ThemedPage';
+
+export default function BluePage() {
+  return <ThemedPage theme={themes.skyblue} />;
+}
